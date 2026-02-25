40b6b35b6f26216cee7fb921c63b677084832c34
castonln <rubixdatcubix@gmail.com>
2026-02-24 23:59:31 -0500

    Final push

--- Project1/data_processing.py
+++ Project1/data_processing.py
@@ -1,10 +1,14 @@
 import pandas as pd
 internet_use = pd.read_csv('data/share-of-individuals-using-the-internet.csv')
 having_friends = pd.read_csv('data/people-who-report-having-friends-or-relatives-they-can-count-on.csv')
+happiness = pd.read_csv('data/happiness-cantril-ladder.csv')
 internet_use_filtered = internet_use.query('Year==2016')
 internet_use_filtered.to_csv('data/share-of-individuals-using-the-internet-2016.csv', index=False)
 
 internet_use_filtered.drop("Year", axis=1, inplace=True)
+happiness.drop(["Year", "Self-reported life satisfaction (Original Year)"], axis=1, inplace=True)
 having_friends.drop("Year", axis=1, inplace=True)
+
 data_combined = pd.merge(internet_use_filtered, having_friends, how='outer')
-data_combined.to_csv('data/combined-data-2016.csv', index=False)
+data_combined_2 = pd.merge(data_combined, happiness, how='outer')
+data_combined_2.to_csv('data/combined-data-2016.csv', index=False)
--- Project1/js/main.js
+++ Project1/js/main.js
@@ -1,180 +1,253 @@
-let data;
-let years = [];
-
-// Margin object with properties for the four directions
-// set the dimensions and margins of the graph
-var margin = { top: 30, right: 30, bottom: 70, left: 60 },
-	width = 1000 - margin.left - margin.right,
-	height = 500 - margin.top - margin.bottom;
+// Shared margins
+const margin = { top: 30, right: 30, bottom: 70, left: 60 };
 
 Promise.all([
-	d3.csv('data/combined-data-2016.csv'),
-	d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
+    d3.csv('data/combined-data-2016.csv'),
+    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
 ])
-	.then(loadData => {
-		console.log('Data loading complete. Work with dataset.');
+    .then(loadData => {
+        const data = loadData[0];
+        const topo = loadData[1];
 
-		var data = loadData[0]
-		var topo = loadData[1]
+        // Parse numeric fields
+        data.forEach(d => {
+            d["People who report having friends or relatives they can count on (%)"] =
+                parseFloat(d["People who report having friends or relatives they can count on (%)"]);
 
-		// Data processing
-		// I thought about catching NaNs here, but it seems easier if I just handle that in 
-		// each plots' function
-		data.forEach(d => {
-			d["People who report having friends or relatives they can count on"] = parseFloat(d["People who report having friends or relatives they can count on"])
-			d["Share of the population using the Internet"] = parseFloat(d["Share of the population using the Internet"])
-		});
+            d["Share of the population using the Internet (%)"] =
+                parseFloat(d["Share of the population using the Internet (%)"]);
 
-		drawBarChart(
-			data,
-			"Entity",
-			"People who report having friends or relatives they can count on",
-			"#friends-or-relatives-bar-chart"
-		);
+            d["Self-reported life satisfaction average (0-10)"] =
+                parseFloat(d["Self-reported life satisfaction average (0-10)"]);
+        });
 
-		drawBarChart(
-			data,
-			"Entity",
-			"Share of the population using the Internet",
-			"#using-the-internet-bar-chart"
-		);
+        // Draw all charts
+        drawBarChart(
+            data,
+            "Entity",
+            "People who report having friends or relatives they can count on (%)",
+            "#friends-or-relatives-bar-chart"
+        );
+
+        drawBarChart(
+            data,
+            "Entity",
+            "Share of the population using the Internet (%)",
+            "#using-the-internet-bar-chart"
+        );
+
+        drawScatterPlot(
+            data,
+            "Share of the population using the Internet (%)",
+            "People who report having friends or relatives they can count on (%)",
+            "#scatterplot-1"
+        );
+
+        drawChoroplethMap(
+            data,
+            "Code",
+            "People who report having friends or relatives they can count on (%)",
+            topo,
+            [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+            d3.schemeBlues[9],
+            "#friends-or-relatives-graph"
+        );
+
+        drawChoroplethMap(
+            data,
+            "Code",
+            "Share of the population using the Internet (%)",
+            topo,
+            [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+            d3.schemeBlues[9],
+            "#using-the-internet-graph"
+        );
+
+        drawBarChart(
+            data,
+            "Entity",
+            "Self-reported life satisfaction average (0-10)",
+            "#life-satisfaction-bar-chart"
+        );
+
+        drawChoroplethMap(
+            data,
+            "Code",
+            "Self-reported life satisfaction average (0-10)",
+            topo,
+            [0,1,2,3,4,5,6,7,8,9,10],
+            d3.schemeBlues[9],
+            "#life-satisfaction-map"
+        );
 
 		drawScatterPlot(
 			data,
-			"Share of the population using the Internet",
-			"People who report having friends or relatives they can count on",
-			"#scatterplot"
+			"Share of the population using the Internet (%)",
+			"Self-reported life satisfaction average (0-10)",
+			"#scatterplot-2"
 		)
 
-		drawChoroplethMap(
-			data,
-			"Code",
-			"People who report having friends or relatives they can count on",
-			topo,
-			[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-			d3.schemeBlues[9],
-			"#friends-or-relatives-graph"
-		)
+        document.getElementById("toggle-friends").addEventListener("click", () => {
+            document.getElementById("friends-container").style.display = "block";
+            document.getElementById("life-container").style.display = "none";
+        });
 
-		drawChoroplethMap(
-			data,
-			"Code",
-			"Share of the population using the Internet",
-			topo,
-			[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-			d3.schemeBlues[9],
-			"#using-the-internet-graph"
-		)
+        document.getElementById("toggle-life").addEventListener("click", () => {
+            document.getElementById("friends-container").style.display = "none";
+            document.getElementById("life-container").style.display = "block";
+        });
+    });
 
-	});
+function drawBarChart(loadData, xAttr, yAttr, elementId) {
 
-function drawBarChart(data, xAttr, yAttr, elementId) {
-	data.sort(function (b, a) {
-		return a[yAttr] - b[yAttr];
-	});
+    // Filter + sort
+    const data = loadData
+        .filter(d => !isNaN(d[yAttr]))
+        .sort((b, a) => a[yAttr] - b[yAttr]);
 
-	const svg = d3.select(elementId)
-		.attr('width', width + margin.left + margin.right)
-		.attr('height', height + margin.top + margin.bottom)
-		.append('g')
-		.attr('transform', `translate(${margin.left}, ${margin.top})`);
+    // Read SVG size from HTML
+    const svg = d3.select(elementId);
+    const svgWidth = +svg.attr("width");
+    const svgHeight = +svg.attr("height");
 
-	// X axis
-	var x = d3.scaleBand()
-		.range([0, width])
-		.domain(data.filter(d => !isNaN(d[yAttr])).map(d => { return d[xAttr]; }))
-		.padding(0.2);
-	svg.append("g")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x))
-		.selectAll("text")
-		.attr("transform", "translate(-10,0)rotate(-45)")
-		.style("text-anchor", "end");
+    const width = svgWidth - margin.left - margin.right;
+    const height = svgHeight - margin.top - margin.bottom;
 
-	// Add Y axis
-	var y = d3.scaleLinear()
-		.domain([0, 100])
-		.range([height, 0]);
-	svg.append("g")
-		.call(d3.axisLeft(y));
+    const g = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-	// Bars
-	svg.selectAll("mybar")
-		.data(data)
-		.enter()
-		.append("rect")
-		.attr("x", function (d) { return x(d[xAttr]); })
-		.attr("y", function (d) { return y(d[yAttr]); })
-		.attr("width", x.bandwidth())
-		.attr("height", function (d) { return height - y(d[yAttr]); })
-		.attr("fill", d3.schemeBlues[6][4])
+    // X scale
+    const x = d3.scaleBand()
+        .range([0, width])
+        .domain(data.map(d => d[xAttr]))
+        .padding(0.2);
 
+    g.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Y scale
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d[yAttr])])
+        .range([height, 0]);
+
+    g.append("g").call(d3.axisLeft(y));
+
+    // X axis label
+    g.append("text")
+        .attr("class", "axis-label")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10)
+        .style("text-anchor", "middle")
+        .text(xAttr);
+
+    // Y axis label
+    g.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 15)
+        .style("text-anchor", "middle")
+        .text(yAttr);
+
+    // Bars
+    g.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", d => x(d[xAttr]))
+        .attr("y", d => y(d[yAttr]))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(d[yAttr]))
+        .attr("fill", d3.schemeBlues[6][4]);
 }
 
 function drawScatterPlot(data, xAttr, yAttr, elementId) {
-	const svg = d3.select(elementId)
-		.attr('width', width + margin.left + margin.right)
-		.attr('height', height + margin.top + margin.bottom)
-		.append('g')
-		.attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-	// Add X axis
-	var x = d3.scaleLinear()
-		.domain([0, 100])
-		.range([0, width]);
-	svg.append("g")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x));
+    const svg = d3.select(elementId);
+    const svgWidth = +svg.attr("width");
+    const svgHeight = +svg.attr("height");
 
-	// Add Y axis
-	var y = d3.scaleLinear()
-		.domain([0, 100])
-		.range([height, 0]);
-	svg.append("g")
-		.call(d3.axisLeft(y));
+    const width = svgWidth - margin.left - margin.right;
+    const height = svgHeight - margin.top - margin.bottom;
 
-	// Add dots
-	svg.append('g')
-		.selectAll("dot")
-		.data(data.filter(d => !isNaN(d[yAttr])))
-		.enter()
-		.append("circle")
-		.attr("cx", function (d) { return x(d[xAttr]); })
-		.attr("cy", function (d) { return y(d[yAttr]); })
-		.attr("r", 1.5)
-		.style("fill", d3.schemeBlues[6][5])
+    const g = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    const x = d3.scaleLinear()
+        .domain([0, 100])
+        .range([0, width]);
+
+    g.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x));
+
+    const y = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height, 0]);
+
+    g.append("g").call(d3.axisLeft(y));
+
+    // X axis label
+    g.append("text")
+        .attr("class", "axis-label")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10)
+        .style("text-anchor", "middle")
+        .text(xAttr);
+
+    // Y axis label
+    g.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 15)
+        .style("text-anchor", "middle")
+        .text(yAttr);
+
+    g.selectAll("circle")
+        .data(data.filter(d => !isNaN(d[yAttr])))
+        .enter()
+        .append("circle")
+        .attr("cx", d => x(d[xAttr]))
+        .attr("cy", d => y(d[yAttr]))
+        .attr("r", 2)
+        .style("fill", d3.schemeBlues[6][5]);
 }
 
 function drawChoroplethMap(loadData, key, value, topo, colorDomain, colorRange, elementId) {
-	var svg = d3.select(elementId),
-		width = +svg.attr("width"),
-		height = +svg.attr("height");
+    var svg = d3.select(elementId),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
 
-	const projection = d3.geoMercator()
-		.scale(70)
-		.center([0, 20])
-		.translate([width / 2, height / 2]);
+    const projection = d3.geoMercator()
+        .scale(70)
+        .center([0, 20])
+        .translate([width / 2, height / 2]);
 
-	const path = d3.geoPath().projection(projection);
+    const path = d3.geoPath().projection(projection);
 
-	// Map CSV values by ISO code
-	const choroplethData = new Map();
-	loadData.forEach(d => choroplethData.set(d[key], d[value]));
+    // Map CSV values by ISO code
+    const choroplethData = new Map();
+    loadData.forEach(d => choroplethData.set(d[key], d[value]));
 
-	const colorScale = d3.scaleThreshold()
-		.domain(colorDomain)
-		.range(colorRange);
+    const colorScale = d3.scaleThreshold()
+        .domain(colorDomain)
+        .range(colorRange);
 
-	svg.append("g")
-		.selectAll("path")
-		.data(topo.features)
-		.join("path")
-		.attr("d", path)
-		.attr("fill", d => {
-			const val = choroplethData.get(d.id);
-			if (val === undefined || isNaN(val)) {
-				return "#cccfdc";
-			}
-			return colorScale(val || 0);
-		});
+    svg.append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .join("path")
+        .attr("d", path)
+        .attr("fill", d => {
+            const val = choroplethData.get(d.id);
+            if (val === undefined || isNaN(val)) {
+                return "#cccfdc";
+            }
+            return colorScale(val || 0);
+        });
 }
